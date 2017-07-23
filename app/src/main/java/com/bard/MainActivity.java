288fe6970468c8326ba0d8ae37bc7bebe10a3498
shaun <shauncarpen@gmail.com>
2017-07-22 20:25:57 -0400

    restructuring

---
+++ app/src/main/java/com/bard/MainActivity.java
@@ -0,0 +1,105 @@
+package com.bard;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import android.net.Uri;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.widget.ListView;
+
+import com.example.shaun.musicapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<song> songList = new ArrayList<song>();
+    private ListView songView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        songView = (ListView)findViewById(R.id.song_list);
+        getSongList();
+
+        Collections.sort(songList, new Comparator<song>(){
+            public int compare(song a, song b){
+                return a.getTitle().compareTo(b.getTitle());
+            }
+        });
+
+        songAdapter songAdt = new songAdapter(this, songList);
+        songView.setAdapter(songAdt);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+//        //noinspection Sim 0plifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void getSongList() {
+        //retrieve song info
+        ContentResolver musicResolver = getContentResolver();
+        Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+
+        if(musicCursor!=null && musicCursor.moveToFirst()){
+            //get columns
+            int titleColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.TITLE);
+            int idColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media._ID);
+            int artistColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.ARTIST);
+            int albumColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.ALBUM);
+            //TODO genre
+            //add songs to list
+            do {
+                long thisId = musicCursor.getLong(idColumn);
+                String thisTitle = musicCursor.getString(titleColumn);
+                String thisArtist = musicCursor.getString(artistColumn);
+                String thisAlbum = musicCursor.getString(albumColumn);
+                songList.add(new song(thisId, thisTitle, thisArtist, thisAlbum, null));
+            }
+            while (musicCursor.moveToNext());
+        }
+    }
+}
---
+++ app/src/main/java/com/bard/song.java
@@ -0,0 +1,37 @@
+package com.bard;
+
+/**
+ * Created by shaun on 7/15/17.
+ */
+
+public class song {
+    private long id;
+    private String title;
+    private String artist;
+    private String album;
+    private String genre;
+
+    public song(long songID, String songTitle, String songArtist, String songAlbum, String songGenre) {
+        id = songID;
+        title = songTitle;
+        artist = songArtist;
+        album = songAlbum;
+        genre = songGenre;
+    }
+
+    public long getID(){
+        return id;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getArtist(){
+        return artist;
+    }
+    public String getAblum(){
+        return album;
+    }
+    public String getGenre(){
+        return genre;
+    }
+}
